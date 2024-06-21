d816424e2140be94b7254ad3da2ec42bff050c73
unknown <tkd.tomy99gmail.com>
2024-06-21 18:01:49 -0300

    modificacion de precio

--- e-commerce/src/componets/productList/ProductList.jsx
+++ e-commerce/src/componets/productList/ProductList.jsx
@@ -3,36 +3,32 @@ import PropTypes from 'prop-types'
 import ProductItem from '../productItem/ProductItem'
 
 
-const ProductList = ({productList,onDeleteProduct }) => {
- 
-  return (
-    <div>
-        {productList.length >0 ?(
-        productList.map((p)=>(
-            <ProductItem
-                key={p.id}
-                id={p.id}
-                name={p.name}
-                brand={p.brand}
-                type={p.type}
-                price={Number(p.price)}
-                img={p.img}  
-                available={p.available}  
-                onDeleteProduct={onDeleteProduct}
-                      
-            />
-        ))
-    ):(
-        <p>No hay nada para mostrar</p>
-    ) }
-    </div>
-  );
+const ProductList = ({ productList, onDeleteProduct, onNewPrice }) => {
+
+    return (
+        <div>
+            {productList.length > 0 ? (
+                productList.map((p) => (
+                    <ProductItem
+                        key={p.id}
+                        product={p}
+                        onDeleteProduct={onDeleteProduct}
+                        onNewPrice={onNewPrice}
+
+                    />
+                ))
+            ) : (
+                <p>No hay nada para mostrar</p>
+            )}
+        </div>
+    );
 };
 
 ProductList.propTypes = {
     productList: PropTypes.array,
     onDeleteProduct: PropTypes.func.isRequired,
-   
+    onNewPrice: PropTypes.func.isRequired,
+
 }
 
 export default ProductList
