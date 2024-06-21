eb8cbfe1fbdd0168c70203456a467de07dab0069
AgusRey04 <reymundez37@gmail.com>
2024-06-21 16:25:26 -0300

    DELETE - arreglado

--- e-commerce/src/componets/home/Home.jsx
+++ e-commerce/src/componets/home/Home.jsx
@@ -1,59 +1,53 @@
+import ProductList from "../productList/ProductList";
+import NewProduct from "../newProduct/NewProduct";
+import { useCallback, useEffect, useState } from "react";
 
-
-import ProductList from '../productList/ProductList'
-import NewProduct from '../newProduct/NewProduct'
-import {  useCallback, useEffect, useState } from 'react'
-
-
-const Home =()=> {
-  const [product, setProduct] = useState([])
-  const [reload, setReload] = useState(true)
-
-
+const Home = () => {
+  const [product, setProduct] = useState([]);
+  const [reload, setReload] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/products", {
-          method: "GET",
-          mode: "cors"
-      })
+      method: "GET",
+      mode: "cors",
+    })
       .then((response) => {
-        if(!response.ok){
-          throw new Error("Error al obtener los productos")
+        if (!response.ok) {
+          throw new Error("Error al obtener los productos");
         }
         return response.json();
       })
-      .then((productData)=>{
-        console.log(productData)
+      .then((productData) => {
+        console.log(productData);
         setProduct(productData);
       })
-      .catch((error)=> {
+      .catch((error) => {
         console.error("Error", error);
-      })
-    
+      });
   }, [reload]);
 
-  const deleteProduct = useCallback( async (id) => {
-    console.log("id en el home",id)
-     try{
-       const response = await fetch("http://localhost:8000/products/$(id)",{
-         method: "DELETE",
-        mode:"cors",
-         headers:{"Content-Type": "application/json"}
-        
-       })
-       if(!response.ok){
-         throw new Error("Error al borrar producto")
-       }else{
-        console.log("Producto eliminado")
-        setProduct(prevProducts => prevProducts.filter(product=> product.id !== id))
-       }
-     }catch(error){
-      alert(error)
-     }
-  },[])
+  const deleteProduct = useCallback(async (id) => {
+    console.log("id en el home", id);
+    try {
+      const response = await fetch(`http://localhost:8000/products/${id}`, {
+        method: "DELETE",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (!response.ok) {
+        throw new Error("Error al borrar producto");
+      } else {
+        console.log("Producto eliminado");
+        setProduct((prevProducts) =>
+          prevProducts.filter((product) => product.id !== id)
+        );
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }, []);
 
-
-  const saveProductDataHandler = async (enteredProductData) =>{
+  const saveProductDataHandler = async (enteredProductData) => {
     const productDto = {
       name: enteredProductData.productName,
       brand: enteredProductData.productBrand,
@@ -64,47 +58,37 @@ const Home =()=> {
       id: 0,
     };
 
-    
-
     try {
-    const  response = await fetch("http://localhost:8000/products", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productDto),
-    });
+      const response = await fetch("http://localhost:8000/products", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(productDto),
+      });
 
-    if(!response.ok){
-      throw new Error("Error al agregar producto")
+      if (!response.ok) {
+        throw new Error("Error al agregar producto");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setProduct(data);
+    } catch (error) {
+      alert(error);
     }
-
-    const data = await response.json();
-    console.log(data)
-    setProduct(data);
-  } catch(error){
-    alert(error)
-    
-  }
-  setReload(!reload)
-
-
-
-}
- 
+    setReload(!reload);
+  };
 
   return (
     <div>
-      <NewProduct onProductDataSaved={saveProductDataHandler}/>
-      <ProductList productList = {product} onDeleteProduct={deleteProduct} />
+      <NewProduct onProductDataSaved={saveProductDataHandler} />
+      <ProductList productList={product} onDeleteProduct={deleteProduct} />
     </div>
-  )
- 
-}
+  );
+};
 
-Home.propTypes = {
- 
-}
+Home.propTypes = {};
 
 export default Home;
