71e6400d1bbb6391c1a415d31e23ec87801dc63a
AgusRey04 <reymundez37@gmail.com>
2024-06-22 19:53:37 -0300

    new component: NewUser |  Changes : Header

--- e-commerce/src/componets/newUser/NewUser.jsx
+++ e-commerce/src/componets/newUser/NewUser.jsx
@@ -1,212 +1,324 @@
 import { useReducer } from "react";
-//import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { Card, Form, Row, Col, Button } from "react-bootstrap";
 
-const NewUser = () => {
-  return <>NewUser</>;
+const initialNewUserForm = {
+  id: 0,
+  firstName: "",
+  lastName: "",
+  username: "",
+  email: "",
+  password: "",
+  contfirmPassword: "",
+  phone: "",
+  addres: "",
+  formValid: false,
 };
 
+const newUserFormReducer = (state, action) => {
+  switch (action.type) {
+    case "FIRST_NAME_UPDATE":
+      return {
+        ...state,
+        firstName: action.value,
+        formValid:
+          action.value &&
+          state.lastName &&
+          state.username &&
+          state.email &&
+          state.password &&
+          state.phone &&
+          state.addres,
+      };
+    case "LAST_NAME_UPDATE":
+      return {
+        ...state,
+        lastName: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.username &&
+          state.email &&
+          state.password &&
+          state.phone &&
+          state.addres,
+      };
+    case "USERNAME_UPDATE":
+      return {
+        ...state,
+        username: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.email &&
+          state.password &&
+          state.phone &&
+          state.addres,
+      };
+    case "EMAIL_UPDATE":
+      return {
+        ...state,
+        email: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.username &&
+          state.password &&
+          state.phone &&
+          state.addres,
+      };
+    case "PASSWORD_UPDATE":
+      return {
+        ...state,
+        password: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.username &&
+          state.email &&
+          state.phone &&
+          state.addres,
+      };
+    case "CONFIRM_PASSWORD_UPDATE":
+      return {
+        ...state,
+        contfirmPassword: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.username &&
+          state.email &&
+          state.phone &&
+          state.addres,
+      };
+    case "PHONE_UPDATE":
+      return {
+        ...state,
+        phone: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.username &&
+          state.email &&
+          state.addres,
+      };
+    case "ADDRESS_UPDATE":
+      return {
+        ...state,
+        addres: action.value,
+        formValid:
+          action.value &&
+          state.firstName &&
+          state.lastName &&
+          state.username &&
+          state.email &&
+          state.password &&
+          state.phone,
+      };
+    case "RESET_FORM":
+      return {
+        initialNewUserForm,
+        formValid: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const NewUser = () => {
+  const [newUserForm, dispatch] = useReducer(
+    newUserFormReducer,
+    initialNewUserForm
+  );
+
+  const submitProductHandler = (event) => {
+    event.preventDefault();
+    console.log();
+
+    if (newUserForm.password === newUserForm.contfirmPassword) {
+      const userDto = {
+        id: 0,
+        firstName: newUserForm.firstName,
+        lastName: newUserForm.lastName,
+        username: newUserForm.username,
+        email: newUserForm.email,
+        password: newUserForm.password,
+        phone: newUserForm.phone,
+        addres: newUserForm.addres,
+        status: true,
+      };
+      console.log(userDto);
+
+      apiPostUser(userDto);
+      dispatch({
+        type: "RESET_FORM",
+      });
+    } else {
+      alert("Las contraseñas no coinciden");
+    }
+  };
+
+  const apiPostUser = async (userDto) => {
+    try {
+      const response = await fetch("http://localhost:8000/users", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userDto),
+      });
+      if (!response.ok) {
+        throw new Error("Error al agregar un usuiario");
+      } else {
+        console.log("Usuario Agregado");
+        console.log(data);
+      }
+      const data = await response.json();
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <>
+      <Card style={{ width: "50rem" }}>
+        <Card.Body>
+          <Form onSubmit={submitProductHandler}>
+            <Row className="mb-3">
+              <Col>
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder=" Ingresar nombre..."
+                  value={newUserForm.firstName}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "FIRST_NAME_UPDATE",
+                      value: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+              <Col>
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder=" Ingresar apellido..."
+                  value={newUserForm.lastName}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "LAST_NAME_UPDATE",
+                      value: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Form.Label>Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=" Ingresar usuario..."
+                value={newUserForm.username}
+                onChange={(e) =>
+                  dispatch({
+                    type: "USERNAME_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+            <Row className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder=" Ingresar email..."
+                value={newUserForm.email}
+                onChange={(e) =>
+                  dispatch({
+                    type: "EMAIL_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+            <Row className="mb-3">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder=" Ingresar contraseña..."
+                value={newUserForm.password}
+                onChange={(e) =>
+                  dispatch({
+                    type: "PASSWORD_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+            <Row className="mb-3">
+              <Form.Label>Confirmar Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder=" Confirmar contraseña..."
+                value={newUserForm.contfirmPassword}
+                onChange={(e) =>
+                  dispatch({
+                    type: "CONFIRM_PASSWORD_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+            <Row className="mb-3">
+              <Form.Label>Teléfono</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=" Ingresar teléfono..."
+                value={newUserForm.phone}
+                onChange={(e) =>
+                  dispatch({
+                    type: "PHONE_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Label>Dirección</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=" Ingresar dirección..."
+                value={newUserForm.addres}
+                onChange={(e) =>
+                  dispatch({
+                    type: "ADDRESS_UPDATE",
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Row>
+            <Button
+              variant="primary"
+              type="submit"
+              disabled={!newUserForm.formValid}
+            >
+              Registrarse
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+NewUser.propTypes = {
+  onNewUserDataSaved: PropTypes.func.isRequired,
+};
 export default NewUser;
-
-// import { useReducer } from "react";
-// import PropTypes from "prop-types";
-// import { Card, Form, Row, Col, Button } from "react-bootstrap";
-
-// const initialProductForm = {
-//   id: 0,
-//   name: "",
-//   price: "",
-//   type: "",
-//   brand: "",
-//   img: "",
-//   formValid: false,
-// };
-
-// const productformReducer = (state, action) => {
-//   switch (action.type) {
-//     case "NAME_UPDATE":
-//       return {
-//         ...state,
-//         name: action.value,
-//         formValid:
-//           action.value && state.price && state.type && state.brand && state.img,
-//       };
-//     case "PRICE_UPDATE":
-//       return {
-//         ...state,
-//         price: action.value,
-//         formValid:
-//           action.value && state.name && state.type && state.brand && state.img,
-//       };
-//     case "TYPE_UPDATE":
-//       return {
-//         ...state,
-//         type: action.value,
-//         formValid:
-//           action.value && state.price && state.name && state.brand && state.img,
-//       };
-//     case "BRAND_UPDATE":
-//       return {
-//         ...state,
-//         brand: action.value,
-//         formValid:
-//           action.value && state.price && state.type && state.name && state.img,
-//       };
-//     case "URL_UPDATE":
-//       return {
-//         ...state,
-//         img: action.value,
-//         formValid:
-//           action.value &&
-//           state.price &&
-//           state.type &&
-//           state.brand &&
-//           state.name,
-//       };
-//     case "RESET_FORM":
-//       return {
-//         ...initialProductForm,
-//         formValid: false,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// const NewProduct = ({ onProductDataSaved }) => {
-//   const [productForm, dispatch] = useReducer(
-//     productformReducer,
-//     initialProductForm
-//   );
-
-//   const handleChangeName = (e) => {
-//     dispatch({
-//       type: "NAME_UPDATE",
-//       value: e.target.value,
-//     });
-//   };
-//   const handleChangePrice = (e) => {
-//     dispatch({
-//       type: "PRICE_UPDATE",
-//       value: e.target.value,
-//     });
-//   };
-//   const handleChangeType = (e) => {
-//     dispatch({
-//       type: "TYPE_UPDATE",
-//       value: e.target.value,
-//     });
-//   };
-//   const handleChangeBrand = (e) => {
-//     dispatch({
-//       type: "BRAND_UPDATE",
-//       value: e.target.value,
-//     });
-//   };
-//   const handleChangeImg = (e) => {
-//     dispatch({
-//       type: "URL_UPDATE",
-//       value: e.target.value,
-//     });
-//   };
-
-//   const submitProductHandler = (e) => {
-//     e.preventDefault();
-//     const productData = {
-//       productName: productForm.name,
-//       productImg: productForm.img,
-//       productPrice: productForm.price,
-//       productType: productForm.type,
-//       productBrand: productForm.brand,
-//       available: true,
-//     };
-//     onProductDataSaved(productData);
-//     dispatch({
-//       type: "RESET_FORM",
-//     });
-//   };
-//   return (
-//     <Card>
-//       <Card.Body>
-//         <Form onSubmit={submitProductHandler}>
-//           <Row className="justify-content-end">
-//             <Col md={6}>
-//               <Form.Group controlId="productName">
-//                 <Form.Label>Nombre</Form.Label>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="Ingrese nombre y breve descripcion"
-//                   onChange={handleChangeName}
-//                   value={productForm.name}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6}>
-//               <Form.Group controlId="productImg">
-//                 <Form.Label>Img</Form.Label>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="Ingrese el enlace de la imagen"
-//                   onChange={handleChangeImg}
-//                   value={productForm.img}
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row className="justify-content-end">
-//             <Col md={6}>
-//               <Form.Group controlId="productPrice">
-//                 <Form.Label>Precio</Form.Label>
-//                 <Form.Control
-//                   type="number"
-//                   placeholder="Ingrese el precio $"
-//                   onChange={handleChangePrice}
-//                   value={productForm.price}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6}>
-//               <Form.Group controlId="productBrand">
-//                 <Form.Label>Marca</Form.Label>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="Ingrese  la Marca del producto"
-//                   onChange={handleChangeBrand}
-//                   value={productForm.brand}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6}>
-//               <Form.Group controlId="productType">
-//                 <Form.Label>Tipo</Form.Label>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="Ingrese el Tipo de producto"
-//                   onChange={handleChangeType}
-//                   value={productForm.type}
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row className="justify-content-end">
-//             <Col md={6} className="d-flex justify-content-end">
-//               <Button
-//                 className="btn btn-success"
-//                 type="sumbit"
-//                 disabled={!productForm.formValid}
-//               >
-//                 Agregar producto
-//               </Button>
-//             </Col>
-//           </Row>
-//         </Form>
-//       </Card.Body>
-//     </Card>
-//   );
-// };
-
-// NewProduct.propTypes = {
-//   onProductDataSaved: PropTypes.func.isRequired,
-// };
-
-// export default NewProduct;
