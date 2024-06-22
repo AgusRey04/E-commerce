ecd0cc98bfd87f7bb2fb7083d20544144b1a0bfd
AgusRey04 <reymundez37@gmail.com>
2024-06-22 01:32:15 -0300

    Login - Logout

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,57 +1,56 @@
-
-import './App.css'
-import Home from './componets/home/Home'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import MaiLayout from './componets/layout/MaiLayout'
-import Login from './componets/login/Login'
-import CartShop from './componets/cartShop/CartShop'
-import ErrorPage from "./componets/errorPage/ErrorPage"
-
-
+import "./App.css";
+import Home from "./componets/home/Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MaiLayout from "./componets/layout/MaiLayout";
+import Login from "./componets/login/Login";
+import CartShop from "./componets/cartShop/CartShop";
+import ErrorPage from "./componets/errorPage/ErrorPage";
+import Logout from "./componets/logout/Logout";
 function App() {
- 
- 
- const router = createBrowserRouter([
+  const router = createBrowserRouter([
     {
-       path: "/",
-       element: (
+      path: "/",
+      element: (
         <MaiLayout>
-          <Home/>
+          <Home />
         </MaiLayout>
-       ),
+      ),
     },
     {
       path: "/login",
       element: (
-       <MaiLayout>
-          <Login/>
-       </MaiLayout>
+        <MaiLayout>
+          <Login />
+        </MaiLayout>
       ),
-   },
-   {
-    path: "/cart",
-    element: (
-     <MaiLayout>
-        <CartShop/>
-     </MaiLayout>
-    ),
-
+    },
+    {
+      path: "/Logout",
+      element: (
+        <MaiLayout>
+          <Logout />
+        </MaiLayout>
+      ),
+    },
+    {
+      path: "/cart",
+      element: (
+        <MaiLayout>
+          <CartShop />
+        </MaiLayout>
+      ),
     },
     {
       path: "*",
       element: (
-       <MaiLayout>
-         <ErrorPage/>
-       </MaiLayout>
+        <MaiLayout>
+          <ErrorPage />
+        </MaiLayout>
       ),
-   }
- 
+    },
+  ]);
 
- ])
-
-  return <RouterProvider router= {router}/>;
-
-  
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
